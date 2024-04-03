8a8c70663d3b5c0b306d205b650679cde014a22b
katisokm10 <130474037+katisokm10@users.noreply.github.com>
2024-04-03 10:37:07 +0200

    added commets

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+// Define a function add that takes two parameters and returns their sum
 function add(a, b) {
 	return a + b;
   }
   
+  // Define a function multiply that takes two parameters and returns their product
   function multiply(a, b) {
 	return a * b;
   }
-
-function internal() {
+  
+  // Define a function internal that calculates the product of two values
+  function internal() {
+	// Calculate the sum of two internal values using the add function
 	const added = this.add(this.internal.a, this.internal.b);
+	// Calculate the product of the sum and another internal value using the multiply function
 	const multiplied = this.multiply(added, this.internal.c);
+	// Log the result to the console
 	console.log(multiplied)
-	return this
-}
-
+	// Return the current context (this)
+	return this;
+  }
+  
 // Not allowed to change below this
 
 const example1 = {
